6712e78a644929f16039e78b0b3928ad565b1062
EssenceCode <123456789+odin@users.noreply.github.com>
2023-11-22 15:53:01 +0800

    created a function and classes created a mergesort to sort and remove duplicates in a array created a Node function that has date and left and right children created a Tree class created a buildTree function

---
+++ src/module/binary-search-tree.js
@@ -0,0 +1,51 @@
+import MergeSort from "./merge-sort";
+
+function Node(data = null, left = null, right = null) {
+    return {
+        data,
+        left,
+        right
+    };
+};
+
+export function buildTree(array, filteredArray = MergeSort(array), start = 0, end = filteredArray.length - 1) { 
+    if(start > end){
+
+        return null;
+    };    
+   
+    const mid = Math.parseInt((start + end) / 2);
+    const root = Node(filteredArray[mid]);
+  
+    
+    root.left = buildTree(array, filteredArray, start, mid - 1);
+    root.right = buildTree(array, filteredArray, mid + 1, end);
+
+    return root;
+};
+
+export const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
+
+export default class Tree {
+    #root;
+
+    constructor(array) {
+        this.#root = buildTree(array);
+    };
+    
+    get root() {
+      return this.#root;
+    };
+}
+
