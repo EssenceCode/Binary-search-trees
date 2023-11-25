ddef1a39ba083df28af0e22410fd04c1760a5c7d
EssenceCode <123456789+odin@users.noreply.github.com>
2023-11-25 14:37:24 +0800

    created a functions that accept a value to delete the same value in the tree

--- src/module/binary-search-tree.js
+++ src/module/binary-search-tree.js
@@ -1,3 +1,4 @@
+
 import MergeSort from "./merge-sort";
 
 function Node(data = null, left = null, right = null) {
@@ -33,7 +34,7 @@ export default class Tree {
     };
 
     insert(value, binaryRoot = this.#root) {
-      const binaryTree = binaryRoot;
+      const node = binaryRoot;
      
       if(binaryRoot.data === value) {
         
@@ -41,29 +42,97 @@ export default class Tree {
       };  
       
       if(binaryRoot.left === null && binaryRoot.data > value) {       
-        binaryTree.left = Node(value);
+        node.left = Node(value);
 
         return true;
       };
 
       if(binaryRoot.right === null && binaryRoot.data < value) {
-        binaryTree.right = Node(value);
+        node.right = Node(value);
 
         return true;
       };
 
       if(binaryRoot.data > value) {
         
-        return this.insert(value, binaryTree.left);
+        return this.insert(value, node.left);
       };
   
       if(binaryRoot.data < value) {
   
-        return this.insert(value, binaryTree.right);
+        return this.insert(value, node.right);
       };
       
-      return binaryTree
+      return node
     };
 
+    remove(value, binaryRoot = this.#root) {
+      const node = binaryRoot;
+      
+      if(binaryRoot !== null) {
+      // if there is no children, means a leaf node 
+        if(binaryRoot.left === null && binaryRoot.right === null) {
+          node.data = null;
+  
+          return true;
+        };
+      // if if there is only children in the left subtree
+        if(binaryRoot.left === null && binaryRoot.data === value) {
+          node.data = node.right.data;
+          node.right = null;
+  
+          return true;
+        };
+      // if if there is only children in the right subtree
+        if(binaryRoot.right === null && binaryRoot.data === value) {
+          node.data = node.left.data;
+          node.left = null;
+          // console.log(node)
+  
+          return true;
+        };
+        // if there are two children
+        if(binaryRoot.left !== null && binaryRoot.right !== null && binaryRoot.data === value) {
+          let predecessor = node;
+          let successor = node.right;
+  
+          while(successor.left !== null) {
+            
+            predecessor = successor;
+            successor = successor.left;
+          };
+          if(predecessor !== node) {
+  
+            predecessor.left = successor.right;
+          };
+  
+          predecessor.right = successor.left;
+  
+          node.data = successor.data;
+  
+          return node
+        };
+        // recursion
+        if(binaryRoot.data > value) {
+
+          return this.remove(value, node.left);
+        };
+  
+        if(binaryRoot.data < value) {
+          
+          return this.remove(value, node.right);
+        };
+
+        return true;
+      };
+
+     if(binaryRoot === null) {
+      console.log("value does not exist")
+      return false
+     };
+      
+     
+      return node;
+    }
 };
 
